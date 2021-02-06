62de213cdc02344fad96d11bf670f7b8f329d2a0
Zakk Haug <zakklikeslamps@gmail.com>
2021-02-05 21:20:20 -0500

    Test Push

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
 $(document).ready(function () {
 
+    //Current Date via moment.JS
+    const currentDay = moment().format("MMMM Do YYYY");
+
+    //API call for Current City Search
+    const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+
+    //MY API key 
     const myKey = "&appid=6277480031780c20e494b59b5119d621";
 
     var cityName = document.getElementById("#city-name");
@@ -11,11 +18,23 @@ $(document).ready(function () {
     var currentWinds = document.getElementById("#winds");
     var uvIndex = document.getElementById("#UV-index");
     var forecast = document.getElementById("#main-weather");
+    var forecastImg = document.getElementById("#weather_image");
 
-    //Current Date via moment.JS
-    const currentDay = moment().format("MMMM Do YYYY");
-    console.log(currentDay);
+    function getCurrentWeather() {
+        var name = cityName.value;
+        var currentWeather = weatherUrl + name + myKey;
 
+        fetch(currentWeather)
+            .then(function (response) {
+                return response.JSON();
+            })
+            .then(function (weatherdata) {
+                currentCity.innerHTML = weatherdata.name + " (" + currentDay + ")";
+                currentTemp.innerHTML = "Temperature : " + weatherdata.main.temp + " F";
+                currentHumidity.innerHTML = "Humidity : " + weatherdata.main.humidity + " %";
+                currentWinds.innerHTML = "Winds : " + weatherdata.main.wind.speed + " mph";
+            });
+    };
 
-
-})
+    searchBtn.addEventListener("click", getCurrentWeather);
+});
